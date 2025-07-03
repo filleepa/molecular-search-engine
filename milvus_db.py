6e98318e6b6774cb10d54971181e5c418700d519
filleepa <78672802+filleepa@users.noreply.github.com>
2025-07-03 09:13:56 +0100

    Downloaded the pubmed SMILES dataset and created a milvus collection called "molecular search"

---
+++ milvus_db.py
@@ -0,0 +1,31 @@
+from pymilvus import connections, CollectionSchema, FieldSchema, DataType, Collection, utility
+
+# create Milvus collection
+MILVUS_HOST = "127.0.0.1"
+MILVUS_PORT = "19530"
+connections.connect(host=MILVUS_HOST, port=MILVUS_PORT)
+
+def create_milvus_collection(collection_name, dim):
+    if utility.has_collection(collection_name):
+        utility.drop_collection(collection_name)
+        
+    fields = [
+        FieldSchema(name="id", dtype=DataType.INT64, description="ids", is_primary=True, auto_id=False),
+        FieldSchema(name="smiles", dtype=DataType.VARCHAR, description="SMILES", max_length=500),
+        FieldSchema(name="embedding", dtype=DataType.BINARY_VECTOR, description="embedding vectors", dim=dim)
+    ]
+    schema = CollectionSchema(fields=fields, description="molecular similarity search")
+    collection = Collection(name=collection_name, schema=schema)
+    
+    index_params = {
+        "index_type":"BIN_FLAT",
+        "params":{"nlist":1024},
+        "metric_type":"JACCARD"
+    }
+    
+    collection.create_index(field_name="embedding", index_params=index_params)
+    
+    return collection
+
+collection = create_milvus_collection("molecular_search", 2048)
+
