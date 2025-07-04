58540ce51ac40bd11bdb1278bf4d821758c21f65
filleepa <78672802+filleepa@users.noreply.github.com>
2025-07-04 10:13:12 +0100

    created the search engine on Milvus via generating fingerprints from SMILES with daylight algorithm and inserting the fingerprints into Milvus

---
+++ search_engine.py
@@ -0,0 +1,20 @@
+from towhee import ops, pipe
+from joblib import load
+from milvus_db import MILVUS_HOST, MILVUS_PORT, collection
+
+df = load("pubchem_df.pkl")
+
+insert_pipe = (pipe.input("df")
+               .flat_map("df", ("smiles", "id"), lambda df: df.values.tolist()) # read tabular data from the df and flatten (smiles and id columns)
+               .map("smiles", "fp", ops.molecular_fingerprinting.rdkit(algorithm="daylight")) # use the daylight algorithm to generate fingerprint with the rdkit operator in towhee hub
+               .map(("id", "smiles", "fp"), "res", ops.ann_insert.milvus_client(host=MILVUS_HOST,
+                                                                                port=MILVUS_PORT,
+                                                                                collection_name="molecular_search")) # inserts molecular fingerprints into Milvus
+               .map("smiles", "smiless", lambda x: print(x))
+               .output("res")
+               )
+
+insert_pipe(df)
+
+collection.flush()
+print(f"Total number of inserted data is {collection.num_entities}.")
