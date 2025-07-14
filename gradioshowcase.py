ec97113471babbc0f972f6a89d78da6921f0b534
filleepa <78672802+filleepa@users.noreply.github.com>
2025-07-14 13:05:07 +0100

    added in similarity score to the output

--- gradioshowcase.py
+++ gradioshowcase.py
@@ -1,7 +1,19 @@
 from milvus_db import MILVUS_HOST, MILVUS_PORT
 from towhee import pipe, ops
-import gradio
-from similarity_search import to_images
+import gradio as gr
+from rdkit import Chem
+from rdkit.Chem import Draw
+from towhee.types.image_utils import from_pil
+
+def to_images_with_score(data, score_list):
+    """Takes a SMILES input and converts it to a towhee.Image object for display. Returns a list of PIL.Image, caption tuples"""
+    imgs = []
+    for smiles, score in zip(data, score_list):
+        mol = Chem.MolFromSmiles(smiles)
+        img = from_pil(Draw.MolToImage(mol))
+        caption = f"{smiles}\nScore: {score:.3f}"
+        imgs.append((img, caption))
+    return imgs
 
 def search_smiles_with_metric(smiles, metric_type):
     search_func = (pipe.input("query_smiles")
@@ -12,17 +24,17 @@ def search_smiles_with_metric(smiles, metric_type):
                                                                                                    limit=5,
                                                                                                    param={"metric_type": metric_type, "nprobe":10},
                                                                                                    output_fields=["smiles"]))
-                   .window_all("query_smiles", "query_smiles", lambda x: to_images(x[:1]))
-                   .window_all("similar_smiles", "similar_smiles", to_images)
-                   .output("similar_smiles")
+                   .window_all(("similar_smiles", "score"), "results", to_images_with_score)
+                   .output("results")
                    )
     return search_func(smiles).to_list()[0][0]
 
-interface = gradio.Interface(search_smiles_with_metric,
-                             [gradio.components.Textbox(lines=1, inputs="CN1C=NC2=C1C(=O)N(C(=O)N2C)C"),
-                              gradio.components.Radio(["JACCARD"])],
-                             [gradio.components.Image(type="pil", label=None) for _ in range(5)],
+interface = gr.Interface(fn=search_smiles_with_metric,
+                             inputs=[gr.components.Textbox(lines=1, label="Query SMILES", value="CN1C=NC2=C1C(=O)N(C(=O)N2C)C"),
+                              gr.components.Radio(["JACCARD"], label="Metric")],
+                             outputs=gr.Gallery(label="Top 5 Similar Structures", columns=5, type="pil"),
                              live=True
                              )
 
-interface.launch(inline=True, share=True)
+if __name__ == "__main__":
+    interface.launch(inline=True, share=True)
